85e983abd12d0769049979f9553b001c9f5ef6ae
root <c780904780904@hotmail.com>
2019-05-25 12:52:44 +0000

    delete review get add rounte

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,18 +5,18 @@ var Review = require("../models/review");
 var middleware = require("../middleware");
 
 // Reviews Index
-router.get("/", function (req, res) {
-    Campground.findById(req.params.id).populate({
-        path: "reviews",
-        options: {sort: {createdAt: -1}} // sorting the populated reviews array to show the latest first
-    }).exec(function (err, campground) {
-        if (err || !campground) {
-            req.flash("error", err.message);
-            return res.redirect("back");
-        }
-        res.render("reviews/index", {campground: campground});
-    });
-});
+// router.get("/", function (req, res) {
+//     Campground.findById(req.params.id).populate({
+//         path: "reviews",
+//         options: {sort: {createdAt: -1}} // sorting the populated reviews array to show the latest first
+//     }).exec(function (err, campground) {
+//         if (err || !campground) {
+//             req.flash("error", err.message);
+//             return res.redirect("back");
+//         }
+//         res.render("reviews/index", {campground: campground});
+//     });
+// });
 
 // Reviews New
 // router.get("/new", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
