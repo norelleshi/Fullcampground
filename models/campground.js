57e5c7a8f9b5600341aa1603d5f521e9817b3e51
root <c780904780904@hotmail.com>
2019-06-13 17:28:28 +0000

    show page UI small change

--- models/campground.js
+++ models/campground.js
@@ -17,7 +17,7 @@ var campgroundSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
-        username: String,
+        username: String
     },
     // comments: [
     //   {
